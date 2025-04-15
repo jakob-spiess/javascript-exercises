4a052941196d547086d225602f6e88e650df1a4f
jakob <jakob.spiess@stud.unibas.ch>
2025-04-16 01:23:07 +0200

    ex4 finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    const finalArray = [];
+    outer: for (let i=0; i < array.length; i++) {
+                for (let j=0; j < arguments.length; j++) {
+                    if (arguments[j] === array[i]) continue outer;
+                }
+            finalArray.push(array[i]);
+            }
+            return finalArray;
 };
 
 // Do not edit below this line
